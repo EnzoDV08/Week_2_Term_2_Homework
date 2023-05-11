a05a9c67beaa39185aa64a73f92e1eb8f32aa883
EnzoDV08 <125486388+EnzoDV08@users.noreply.github.com>
2023-05-11 20:05:54 +0200

    Add files via upload

---
+++ Week 2 Homework/main.js
@@ -0,0 +1,48 @@
+//Exercise One:
+var firstName = "Enzo"
+let surename = "De Vittorio"
+let integer = 20
+const subject = "Develpment"
+
+let message = "Hello , my name is " + firstName + " " + surename + ", I am " + integer + " years old , and I am studing " + subject + ".";
+
+console.log(message)
+
+//Exercise Two:
+let numOne = 10;
+let numTwo = 20;
+let numThree = 5;
+let numFour = 10;
+
+let answer = numOne + numTwo / numThree * numFour;
+
+console.log(answer)
+
+//Exercise three:
+let timmyMoney = 10;
+let timmyTax = 0.25;
+let timmyTaxTotal = timmyMoney * timmyTax;
+
+let susanMoney = 25;
+let susanTax = 0.26;
+let susanTaxTotal = susanMoney * susanTax;
+
+const tax = timmyMoney + susanMoney;
+console.log("The Tax Man will claim R" + tax.toFixed(0) + " from Timmy & Susan.");
+
+//Exercise four:
+let johnMarks = 60 + 7;
+let timMarks = 70 + 7;
+let alfiemarks = 75 + 7;
+let gwenmarks = 43 + 7;
+
+let allMarks = johnMarks + timMarks + alfiemarks + gwenmarks;
+let averageMarks = allMarks / 4;
+console.log("The students updated value is: " + averageMarks + "/100");
+
+
+
+
+
+
+
